7b9d6486a12a623b09d5382e802595b5c30ef535
Cameron <camster91@gmail.com>
2026-02-27 09:01:54 -0500

    fix: allow Google Fonts in Content Security Policy (style-src + font-src)

--- next.config.js
+++ next.config.js
@@ -14,9 +14,9 @@ const securityHeaders = [
     value: [
       "default-src 'self'",
       "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
-      "style-src 'self' 'unsafe-inline'",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: blob:",
-      "font-src 'self'",
+      "font-src 'self' https://fonts.gstatic.com",
       "connect-src 'self'",
       "frame-ancestors 'none'",
     ].join('; '),
